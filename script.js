76a4039fa8edbefb3cdf60ff189eba0f107351dd
AlessioRomano95 <romanalex400@gmail.com>
2024-03-12 15:58:28 +0100

    [Ri ADD] file

--- script.js
+++ script.js
@@ -1,119 +1,83 @@
-///////////////////////////// 
-//Ho problemi di connessione da 1 settimana ( problemi alla centralina vodaphone del quartiere ) ieri notte lo avevano fixato, ma oggi pomeriggio ha smesso di nuovo di funzionare,
-// non avevo più internet a casa e ho finito i gb dal telefono da giorni.
-// purtroppo non ho potuto fare nemmeno un commit, ma per evitare di rimanere indietro ho continuato a lavorare//
-//spero non sia un grossissimo problema ma non potevo permettermi di fermarmi ho troppi esercizi ancora non finiti//
+console.log('js ok')
 
+// 1
+const itemsWrapper = document.querySelector('.items-wrapper');
+const topC = document.querySelector('.top');
+const botC = document.querySelector('.bottom');
 
+topC.classList.add('hide')
 
+// 2
 
 const images = [
-  {
-      url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
-      title: 'Svezia',
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
-  },
-  {
-      url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
-      title: 'Perù',
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
-  },
-  {
-      url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
-      title: 'Chile',
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
-  },
-  {
-      url: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg',
-      title: 'Argentina',
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
-  },
-  {
-      url: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop',
-      title: 'Colombia',
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
-  },
-];
+  'img/01.webp',
+  'img/02.webp',
+  'img/03.webp',
+  'img/04.webp',
+  'img/05.webp',
+]
 
+let counterImg = 0;
 
-// Elementi DOM
-const myNext = document.querySelector(".my-next");
-const myPrevius = document.querySelector(".my-previous");
-const myCarousel = document.querySelector(".my-carousel-images");
-const myThumbnails = document.querySelector(".my-thumbnails");
-const btnsx = document.getElementById("btnsx");
-const btndx = document.getElementById("btndx");
+// 3
+for(let i = 0; i < images.length; i++){
+  const img = images [i];
+  itemsWrapper.innerHTML += `<img class="img hide" src="${img}">`
+}
 
-let currentIndex = 0;
-let interval;
+// 4
+const itemsCollection = document.getElementsByClassName('img');
+itemsCollection[counterImg].classList.remove('hide');
 
-
-// Carica le immagini nel carosello
-images.forEach((image, index) => {
-  myCarousel.innerHTML += `
-      <div class="my-carousel-item">
-          <img class="img-fluid" src="${image.url}" alt="${image.title}">
-          <div class="item-description px-3">
-              <h2>${image.title}</h2>
-              <p>${image.description}</p>
-          </div>
-      </div>
-  `;
-  myThumbnails.innerHTML += `
-      <div class="my-thumbnail" data-index="${index}">
-          <img class="img-fluid" src="${image.url}" alt="${image.title}">
-      </div>
-  `;
-});
-
-// Inizializza il carosello
-const carouselItems = document.querySelectorAll('.my-carousel-item');
-const thumbnails = document.querySelectorAll('.my-thumbnail');
-toggleActive(currentIndex);
-
-// event listener per il pulsante "Successivo"
-myNext.addEventListener("click", () => { 
-  clickNext();
-});
-
-// event listener per il pulsante "Precedente"
-myPrevius.addEventListener("click", () => { 
-  clickPrev();
-});
-
-// event listener per le miniature
-thumbnails.forEach((thumb, index) => {
-  thumb.addEventListener('click', () => {
-      currentIndex = index;
-      toggleActive(currentIndex);
-  });
-});
-
-// Funzione per passare all'immagine successiva
-function clickNext() {
-  currentIndex++;
-  if (currentIndex === images.length) { 
-      currentIndex = 0;
+// 5
+topC.addEventListener('click', function(){    
+  botC.classList.remove('hide')
+  itemsCollection[counterImg].classList.add('hide');
+  counterImg--;
+  if(counterImg === 0){
+    topC.classList.add('hide')
   }
-  toggleActive(currentIndex);
-}
 
-// Funzione per passare all'immagine precedente
-function clickPrev() {
-  currentIndex--;
-  if (currentIndex < 0) { 
-      currentIndex = images.length - 1;
+  // 6
+  itemsCollection[counterImg].classList.remove('hide');
+})
+
+// 7
+botC.addEventListener('click', function(){
+  topC.classList.remove('hide')
+  itemsCollection[counterImg].classList.add('hide');
+  counterImg++;
+
+  itemsCollection[counterImg].classList.remove('hide');
+  if(counterImg === images.length -1){
+    botC.classList.add('hide')
   }
-  toggleActive(currentIndex);
+})
+
+//8 autoplay
+function nextSlide() {
+  itemsCollection[counterImg].classList.add('hide');
+  counterImg++;
+  if (counterImg === images.length) {
+      counterImg = 0;
+  }
+  itemsCollection[counterImg].classList.remove('hide');
 }
 
-// Funzione per impostare l'elemento attivo
-function toggleActive(index) {
-  carouselItems.forEach(item => item.classList.remove('active'));
-  thumbnails.forEach(thumb => thumb.classList.remove('active'));
-  carouselItems[index].classList.add('active');
-  thumbnails[index].classList.add('active');
+//9 Funzione per avviare l'autoplay
+function startAutoplay() {
+  interval = setInterval(nextSlide, 3000);
 }
 
-// Avvia il ciclo di scorrimento automatico
-interval = setInterval(clickNext, 3000);
+//10 Avvia l'autoplay all'avvio dello script
+startAutoplay();
+
+//11 Ferma l'autoplay quando si passa sopra a una slide
+itemsWrapper.addEventListener('mouseover', function() {
+  clearInterval(interval);
+});
+
+//12 Riprende l'autoplay quando si esce dalla slide
+itemsWrapper.addEventListener('mouseout', function() {
+  startAutoplay();
+});
